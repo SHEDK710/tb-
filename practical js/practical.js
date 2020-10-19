e22071fcb53f6e9a71ad764f71624bd9db2a1eb0
SHEDK710 <55700743+SHEDK710@users.noreply.github.com>
2020-10-19 21:57:11 +0800

    Add files via upload

---
+++ practical js/practical.js
@@ -0,0 +1,61 @@
+function addLoadEvent(func) {
+    var oldonload = window.onload;
+    if (typeof window.onload != 'function') {
+        window.onload = func;
+    } else {
+        window.onload = function() {
+            oldonload();
+            func();
+        }
+    }
+}
+
+function insertAfter(newElement, targetElement) {
+    var parent = targetElement.parentNode;
+    if (parent.lastChild == targetElement) {
+        parent.appendChild(newElement);
+    } else {
+        parent.insertBefore(newElement, targetElement.nextSibling);
+    }
+}
+
+function moveElement(elementId, final_x, final_y, interval) {
+    if (!document.getElementById) return false;
+    if (!document.getElementById(elementId)) return false;
+    var elem = document.getElementById(elementId);
+    var xpos = parseInt(elem.style.left);
+    var ypos = parseInt(elem.style.tpo);
+    var dist = 0;
+    if (elem.movement) {
+        clearTimeout(elem.movement);
+    }
+    if (!elem.style.left) {
+        elem.style.left = "0px";
+    }
+    if (!elem.style.top) {
+        elem.style.top = "0px";
+    }
+    if (xpos == final_x && ypos == final_y) {
+        return true;
+    }
+    if (xpos < final_x) {
+        dist = Math.ceil((final_x - xpos) / 10);
+        xpos = xpos + dist;
+    }
+    if (xpos > final_x) {
+        dist = Math.ceil((xpos - final_x) / 10);
+        xpos = xpos - dist;
+    }
+    if (ypos < final_y) {
+        dist = Math.ceil((final_y - ypos) / 10);
+        ypos = ypos + dist;
+    }
+    if (ypos > final_y) {
+        dist = Math.ceil((ypos - final_y) / 10);
+        ypos = ypos - dist;
+    }
+    elem.style.left = xpos + "px";
+    elem.style.top = ypos = "px";
+    var repeat = "moveElement('" + elementId + "'," + final_x + "," + final_y + "," + interval + ")";
+    elem.movement = setTimeout(repeat, interval);
+}
